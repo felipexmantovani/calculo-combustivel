775aab444cff7005ce4ce6d14334cdbae91dad61
Carlos Henrique Mantovani <mantovanicarloshenrique@gmail.com>
2023-10-30 20:32:56 -0300

    estilização dos scripts

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,36 +1,36 @@
-const BTN_DARK = document.getElementById('btn');
-const BTN_LIGHT = document.getElementById('btn2');
-const THEME_KEY = 'theme';
-const THEME_DARK = 'dark';
-const THEME_LIGHT = 'light';
+const BTN_DARK = document.getElementById("btn");
+const BTN_LIGHT = document.getElementById("btn2");
+const THEME_KEY = "theme";
+const THEME_DARK = "dark";
+const THEME_LIGHT = "light";
 
 function setThemeDark() {
-  document.body.classList.remove(THEME_LIGHT);
-  document.body.classList.add(THEME_DARK);
-  localStorage.setItem(THEME_KEY, THEME_DARK);
+	document.body.classList.remove(THEME_LIGHT);
+	document.body.classList.add(THEME_DARK);
+	localStorage.setItem(THEME_KEY, THEME_DARK);
 }
 
 function setThemeLight() {
-  document.body.classList.remove(THEME_DARK);
-  document.body.classList.add(THEME_LIGHT);
-  localStorage.setItem(THEME_KEY, THEME_LIGHT);
+	document.body.classList.remove(THEME_DARK);
+	document.body.classList.add(THEME_LIGHT);
+	localStorage.setItem(THEME_KEY, THEME_LIGHT);
 }
 
 function checkTheme() {
-  const LOCALSTORAGE_THEME = localStorage.getItem(THEME_KEY);
-  if (LOCALSTORAGE_THEME) {
-    LOCALSTORAGE_THEME === THEME_LIGHT ? setThemeLight() : setThemeDark();
-  } else {
-    window.matchMedia('(prefers-color-scheme: dark)').matches ? setThemeDark() : setThemeLight();
-  }
+	const LOCALSTORAGE_THEME = localStorage.getItem(THEME_KEY);
+	if (LOCALSTORAGE_THEME) {
+		LOCALSTORAGE_THEME === THEME_LIGHT ? setThemeLight() : setThemeDark();
+	} else {
+		window.matchMedia("(prefers-color-scheme: dark)").matches ? setThemeDark() : setThemeLight();
+	}
 }
 
 checkTheme();
 
-BTN_DARK.addEventListener('change', (e) => {
-  e.target.checked ? setThemeDark() : setThemeLight();
+BTN_DARK.addEventListener("change", (e) => {
+	e.target.checked ? setThemeDark() : setThemeLight();
 });
 
-BTN_LIGHT.addEventListener('change', (e) => {
-  e.target.checked ? setThemeLight() : setThemeDark();
+BTN_LIGHT.addEventListener("change", (e) => {
+	e.target.checked ? setThemeLight() : setThemeDark();
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,30 @@
-$('.input').mask('#,##0.00', { reverse: true });
-const FORM = document.querySelector('form');
-FORM.addEventListener('submit', (event) => {
-  event.preventDefault();
-  hideToast();
+/* eslint-disable no-undef */
+$(".input").mask("#,##0.00", { reverse: true });
+const FORM = document.querySelector("form");
+FORM.addEventListener("submit", (event) => {
+	event.preventDefault();
+	hideToast();
 
-  const INPUTS = FORM.querySelectorAll('.input');
-  const INPUT_EMPTY = Object.values(INPUTS).filter(input => input.value.trim() === '');
+	const INPUTS = FORM.querySelectorAll(".input");
+	const INPUT_EMPTY = Object.values(INPUTS).filter(input => input.value.trim() === "");
 
-  if (INPUT_EMPTY.length > 0) {
-    showToast(`Informe o valor do campo ${INPUT_EMPTY[0].placeholder}`, TOAST_TYPE.WARNING);
-    return;
-  }
-  showToast(`Abasteça com ${getFuelThatCompensates()}`, TOAST_TYPE.SUCESS);
+	if (INPUT_EMPTY.length > 0) {
+		showToast(`Informe o valor do campo ${INPUT_EMPTY[0].placeholder}`, TOAST_TYPE.WARNING);
+		return;
+	}
+	showToast(`Abasteça com ${getFuelThatCompensates()}`, TOAST_TYPE.SUCESS);
 });
 
 function getFuelThatCompensates() {
 
-  const VALOR_ETANOL = parseFloat(document.getElementById('inputEtanol').value)
-  const VALOR_GASOLINA = parseFloat(document.getElementById('inputGasolina').value)
-  const PORCENTAGEM = VALOR_GASOLINA * 0.75
+	const VALOR_ETANOL = parseFloat(document.getElementById("inputEtanol").value);
+	const VALOR_GASOLINA = parseFloat(document.getElementById("inputGasolina").value);
+	const PORCENTAGEM = VALOR_GASOLINA * 0.75;
 
-  if (PORCENTAGEM >= VALOR_ETANOL) {
-    return "Etanol"
-  }
-  else {
-    return "Gasolina"
-  }
+	if (PORCENTAGEM >= VALOR_ETANOL) {
+		return "Etanol";
+	}
+	else {
+		return "Gasolina";
+	}
 }
